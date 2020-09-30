8ba6b67548818df6400714589012db611487f17c
vincentvvv <vincent.vuong@compass.com>
2020-09-30 15:20:59 -0400

    add pods

---
+++ CarFax/CarsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CarsViewModel.swift
+//  CarFax
+//
+//  Created by Vincent Vuong on 9/30/20.
+//  Copyright © 2020 Vincent Vuong. All rights reserved.
+//
+
+import Foundation
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '11.0'
+
+target 'CarFax' do
+  pod 'Snail', '0.7.0'
+  pod 'SnapKit', '~> 5.0.0'
+end
